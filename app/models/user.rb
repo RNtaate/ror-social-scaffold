07dfae8a12a7b86d282c91ab1ca87f2ad9599d57
ershadul1 <ershadul.rayhan@gmail.com>
2020-10-08 16:50:22 +0600

    Add associations between user and friendship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,10 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  has_many :friended_users, foreign_key: :friender_id, class_name: 'Friendship'
+  has_many :friendees, through: :friended_users
+
+  has_many :friending_users, foreign_key: :friendee_id, class_name: 'Friendship'
+  has_many :frienders, through: :friending_users
 end
