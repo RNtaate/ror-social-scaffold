514df33e6c47ce2d0b789972773d1d44bf207be9
RNtaate <royhyde14@gmail.com>
2020-10-08 17:33:49 +0300

    Update frienship_checker method in application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,13 +22,27 @@ module ApplicationHelper
 
   def friendship_checker(user, friendship_id)
     return if current_user == user
-    
+
     if current_user.friends.include?(user)
-      content_tag(:p, 'Friends')
+      content_tag :span do
+        content_tag(:b, 'Friends ') +
+          content_tag(:small,
+                      (link_to 'Unfriend', friendship_path(friendship_id),
+                               method: :delete, data: { confirm: 'Are you sure you want to unfriend this person?' }))
+      end
+
     elsif current_user.pending_friends.include?(user)
-      content_tag(:p, 'Friend Request sent ...')
+      content_tag(:b, 'Friend Request sent ...')
+
     elsif current_user.friend_requests.include?(user)
-      link_to 'Accept request', friendship_path(friendship_id), method: :put
+      content_tag :span do
+        content_tag(:b, (link_to 'Accept request', friendship_path(friendship_id), method: :put)) +
+          content_tag(:span, ' ') +
+          content_tag(:small,
+                      (link_to 'Reject request', friendship_path(friendship_id),
+                               method: :delete, data: { confirm: 'Are you sure you want to reject this request?' }))
+      end
+
     else
       link_to 'Add as friend', friendships_path(friendee_id: user.id), method: :post
     end
