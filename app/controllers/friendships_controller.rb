2e869108a9157b6e6d2f07f62ea94f6ce3bf6a10
RNtaate <royhyde14@gmail.com>
2020-10-08 15:33:53 +0300

    Add create and update methods to friendships controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,23 @@
 class FriendshipsController < ApplicationController
+  include ApplicationHelper
+  def new; end
+
+  def create
+    @friendship = Friendship.new
+    @friendship.friender_id = current_user.id
+    @friendship.friendee_id = params[:friendee_id]
+    @friendship.friendship_id = friendship_id_generator(current_user.id, params[:friendee_id].to_i)
+
+    @friendship.save unless Friendship.where(friendship_id: @friendship.friendship_id).exists?
+
+    redirect_to users_path
+  end
+
+  def update
+    @friendship = Friendship.find_by(friendship_id: params[:id])
+    @friendship.status = true
+    @friendship.save
+
+    redirect_to users_path
+  end
 end
