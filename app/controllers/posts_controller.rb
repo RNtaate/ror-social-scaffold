e36ade2a6cc9e080a86108ee6ee68bf60819a733
ershadul1 <ershadul.rayhan@gmail.com>
2020-10-13 15:02:24 +0600

    Update timeline_posts method in posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,8 @@ class PostsController < ApplicationController
   private
 
   def timeline_posts
-    @timeline_posts ||= Post.all.ordered_by_most_recent.includes(:user)
+    friend_list = current_user.friends + [current_user]
+    @timeline_posts ||= Post.where(user_id: friend_list).ordered_by_most_recent.includes([:user],[:comments])
   end
 
   def post_params
